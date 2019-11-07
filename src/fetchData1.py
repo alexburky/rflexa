d987963ab9bff811846007437c089d2959169b41
Alex Burky <burk4y@gmail.com>
2019-11-07 14:06:44 -0500

    Some bug fixes. More to do...

--- src/fetchData1.py
+++ src/fetchData1.py
@@ -4,13 +4,13 @@ from obspy.clients.fdsn import Client
 from obspy.taup import TauPyModel
 import matplotlib
 import numpy as np
+import seisutils as su
 matplotlib.use('Qt4Agg')
 import matplotlib.pyplot as plt
-# import seisutils as su
 
 # ------------------------------------------------------------------------------------------
 # fetchData.py
-# This code fetches seismic data for events within a give time window using ObsPy
+# This script fetches seismic data for events within a give time window using ObsPy
 # and saves the data into .sac files.
 # TO DO: Add an 'example' where you fetch a small handful of events and then run them
 # through the entire receiver function analysis for reproducibility and teaching purposes
@@ -65,7 +65,7 @@ for i in range(0, nstats):
     stla = inv.networks[0].stations[i].latitude
     stlo = inv.networks[0].stations[i].longitude
     # Fetch relevant events in time-window during which station was operational
-    catalog = client.get_events(starttime=t0, endtime=tf, minmagnitude=8, maxmagnitude=9, latitude=stla,
+    catalog = client.get_events(starttime=t0, endtime=tf, minmagnitude=6.5, maxmagnitude=6.6, latitude=stla,
                                 longitude=stlo, minradius=30, maxradius=90)
     nevents = len(catalog.events)
     # Initialize list of events used for bulk request
@@ -75,7 +75,9 @@ for i in range(0, nstats):
         teq = catalog.events[j].origins[0].time
         bulk.append((ntwk, stat, loc, chan, teq, teq+60*60))
     # Fetch the data!
+    print(bulk)
     st = client.get_waveforms_bulk(bulk, attach_response=True)
+    l = 0
     for j in range(0, len(st)):
         teq = st[j].meta.starttime
         # Check which instrument response to use for given event
@@ -91,9 +93,52 @@ for i in range(0, nstats):
         evid = st[j].meta.starttime.isoformat().replace('-', '.').replace('T', '.').replace(':', '.').split('.')[:-1]
         evid.extend([ntwk, stat, loc, evchan, 'sac'])
         evid = ".".join(evid)
-        # st[j].stats.sac.stla = stla
-        # st[j].stats.sac.stlo = stlo
-        st[j].write("/Users/aburky/PycharmProjects/bermudaRFs/data/bigQuakes/" + evid, format='SAC')
+        # Add station-specific metadata to SAC files
+        st[j].stats.sac = {}
+        st[j].stats.sac.stla = stla
+        st[j].stats.sac.stlo = stlo
+
+        # NEED TO ADD A CHECK TO MAKE SURE ALL 3 CHANNELS HAVE DATA!
+
+        # Add event-specific metadata to SAC files
+        nchans = len(st)/3
+        if l < nevents:
+            st[j].stats.sac.evla = catalog.events[-(l+1)].origins[0].latitude
+            st[j].stats.sac.evlo = catalog.events[-(l+1)].origins[0].longitude
+            st[j].stats.sac.evdp = catalog.events[-(l+1)].origins[0].depth
+            # Calculate great circle distance and back-azimuth
+            gcarc, baz = su.haversine(stla, stlo, st[j].stats.sac.evla, st[j].stats.sac.evlo)
+            st[j].stats.sac.gcarc = gcarc
+            st[j].stats.sac.baz = baz
+            # Get theoretical P arrival time, and assign to header 'T0'
+            model = TauPyModel(model="iasp91")
+            phases = ["P"]
+            arrivals = model.get_travel_times(source_depth_in_km=st[j].stats.sac.evdp/1000.0, distance_in_degree=gcarc,
+                                              phase_list=phases)
+            st[j].stats.sac.t0 = arrivals[0].time
+            print(arrivals[0].time, evid, catalog.events[-(l + 1)].origins[0].time, l)
+            # Write the trace to a SAC file
+            st[j].write("/Users/aburky/PycharmProjects/bermudaRFs/data/rfQuakes/" + evid, format='SAC')
+            l += 1
+        else:
+            l = 0
+            st[j].stats.sac.evla = catalog.events[-(l+1)].origins[0].latitude
+            st[j].stats.sac.evlo = catalog.events[-(l+1)].origins[0].longitude
+            st[j].stats.sac.evdp = catalog.events[-(l+1)].origins[0].depth
+            # Calculate great circle distance and back-azimuth
+            gcarc, baz = su.haversine(stla, stlo, st[j].stats.sac.evla, st[j].stats.sac.evlo)
+            st[j].stats.sac.gcarc = gcarc
+            st[j].stats.sac.baz = baz
+            # Get theoretical P arrival time, and assign to header 'T0'
+            model = TauPyModel(model="iasp91")
+            phases = ["P"]
+            arrivals = model.get_travel_times(source_depth_in_km=st[j].stats.sac.evdp/1000.0, distance_in_degree=gcarc,
+                                              phase_list=phases)
+            st[j].stats.sac.t0 = arrivals[0].time
+            print(arrivals[0].time, evid, catalog.events[-(l+1)].origins[0].time, l)
+            # Write the trace to a SAC file
+            st[j].write("/Users/aburky/PycharmProjects/bermudaRFs/data/rfQuakes/" + evid, format='SAC')
+            l += 1
 
 # Plot some data for fun
 plt.plot(st[0].data, 'k', linewidth=0.25)
