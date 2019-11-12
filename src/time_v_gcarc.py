42a317810c9dc2dec52792d88c79cd0bd9c3be57
Alex Burky <burk4y@gmail.com>
2019-11-12 08:39:48 -0500

    Added initial plotting script

--- src/time_v_gcarc.py
+++ src/time_v_gcarc.py
@@ -4,8 +4,6 @@ from scipy import signal
 import matplotlib
 matplotlib.use('Qt4Agg')
 import matplotlib.pyplot as plt
-# import plotly.graph_objects as go
-# from plotly.offline import iplot
 
 # This script makes a contour plot of receiver function data in the time domain as a function of epicentral
 # distance
@@ -32,37 +30,44 @@ for i in range(0, len(rfs)):
         gcarcs[j] = [rfs[i].stats.sac.gcarc, i]
         j += 1
 
-# print(np.sort(gcarcs, axis=0)[:, 0])
+gcarcs = np.array(gcarcs)
+sorted_gcarcs = gcarcs[gcarcs[:, 0].argsort()]
 
 # Resample data for consistent plotting
 npmax = np.max(npts)
 dt = np.min(delta)
+norm_rfs = []
 for i in range(0, len(rfs)):
     if rfs[i].stats.sac.npts != npmax:
         rfs[i].data = signal.resample(rfs[i].data, npmax)
+    norm_rfs.append(i)
+    norm_rfs[i] = np.array(rfs[i].data/np.max(rfs[i].data))
+norm_rfs = np.squeeze(norm_rfs)
 
-x = np.arange(0, npmax*dt, dt)
-y = np.sort(gcarcs, axis=0)[:, 0]
-X, Y = np.meshgrid(x, y)
-z = []
+# Construct vectors for plotting
+shifted_rfs = []
+for i in range(0, len(sorted_gcarcs)):
+    shifted_rfs.append(i)
+    shifted_rfs[i] = sorted_gcarcs[i][0] + \
+        (rfs[int(sorted_gcarcs[i][1])].data/np.max(rfs[int(sorted_gcarcs[i][1])].data))
+# Bin receiver functions in two-degree wide bins
+bins = np.arange(30, 90, 1)
+rf_bin = []
 j = 0
-for i in np.sort(gcarcs, axis=0)[:, 1]:
-    z.append(j)
-    z[j] = np.array(rfs[int(i)].data/np.max(rfs[int(i)].data))
+for i in bins:
+    binmin = i - 1
+    binmax = i + 1
+    idx = np.where(np.logical_and(sorted_gcarcs[:, 0] >= binmin, sorted_gcarcs[:, 0] < binmax))
+    idxs = sorted_gcarcs[idx, 1].astype(int)
+    rf_bin.append(j)
+    rf_bin[j] = i + np.sum(norm_rfs[idxs, :], axis=0)/len(idxs)
+    if rf_bin[j].size == 0:
+        rf_bin[j] = i + np.zeros((rf_bin[j].shape[0] + 1, rf_bin[j].shape[1]))
     j += 1
 
-# Plot
-# fig = plt.subplot()
-# CS = fig.contourf(X, Y, z)
-plt.contourf(X, Y, z, 20, cmap='RdGy_r', levels=np.linspace(-0.05, 0.1, 20))
-plt.xlim(30, 120)
-plt.colorbar()
+x = np.arange(0, npmax*dt, dt)
+# for i in range(0, len(shifted_rfs)):
+#    plt.plot(x, shifted_rfs[i], 'k', linewidth=0.25)
+for i in range(0, len(rf_bin)):
+    plt.plot(x, rf_bin[i][0], 'k', linewidth=0.25)
 plt.show()
-# cbar = fig.colorbar(CS)
-
-# Using plotly (slow, but nice?)
-# contour = go.Contour(x=x, y=y, z=z, contours=dict(start=0, end=0.1, size=0.01), line_smoothing=0.85)
-# data = [contour]
-# fig = go.Figure(data=data)
-# fig.update_xaxes(range=[30, 100])
-# fig.show(renderer='chrome')
