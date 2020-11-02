21d419ececf6fa29bb9ccbf0917c02100ccbb4e7
Alex Burky <burk4y@gmail.com>
2020-11-02 15:16:35 -0500

    Small updates

--- matlab/fetchRFQuakes.m
+++ matlab/fetchRFQuakes.m
@@ -5,57 +5,58 @@ clear,clc
 % Define the network, station that you would like to fetch data for
 network = 'TA';
 station = 'O61A';
-% network = 'IU';
-% station = 'BBSR';
+% network = 'II';
+% station = 'SACV';
 location = '*';
 channel = 'BH*';
 
 ch = irisFetch.Channels('RESPONSE',network,station,location,channel);
 
-% So, it looks like you can't get a RESP file for a station that has
-% been updated without getting the RESP information for all prior
-% periods that station was operational.
-% Need to come up with a way to deal with this - looks like we will need
-% to use some datestr manipulation!
-% Just save the one RESP file that has it all...
+% Options to add:
+% - Directory where you would like to save the output data
+% - RESP, or PZ files?
 
-% Loop over all channels, and then find the latest operational date for
-% each channel (BHE,BHN,BHZ,etc.) - thenn fetch and save the RESP file
-% for this time period!
-
-% Loop over each channel and get the RESP file
-for i = 1:length(ch)
-    t1 = ch(i).StartDate;
-    t2 = datetime(t1) + milliseconds(1);
-    formatOut = 'yyyy-mm-dd HH:MM:SS.FFF';
-    t2 = datestr(t2,formatOut);
-    % Fetch the RESP file
-    re = irisFetch.Resp(network,station,location,ch(i).ChannelCode,t1,t2);
-    % Format the RESP filename
-    respFile = sprintf('RESP.%s.%s.%s.%s.%d',network,station,...
-               ch(i).LocationCode,ch(i).ChannelCode,i);
-    % Save the RESP file
-    fID = fopen(respFile,'w');
-    fprintf(fID,re);
-    fclose(fID);
-end
+% % Loop over each channel and get the RESP file
+% for i = 1:length(ch)
+%     t1 = ch(i).StartDate;
+%     t2 = datetime(t1) + milliseconds(1);
+%     formatOut = 'yyyy-mm-dd HH:MM:SS.FFF';
+%     t2 = datestr(t2,formatOut);
+%     % Fetch the RESP file
+%     re = irisFetch.Resp(network,station,location,ch(i).ChannelCode,t1,t2);
+%     % Format the RESP filename
+%     respFile = sprintf('RESP.%s.%s.%s.%s.%d',network,station,...
+%                ch(i).LocationCode,ch(i).ChannelCode,i);
+%     % Save the RESP file
+%     fID = fopen(respFile,'w');
+%     fprintf(fID,re);
+%     fclose(fID);
+% end
 
 % Loop over each channel and make the PZ file
 for i = 1:length(ch)
-    % t1 = ch(i).StartDate;
-    % t2 = datetime(t1) + milliseconds(1);
-    % formatOut = 'yyyy-mm-dd HH:MM:SS.FFF';
-    % t2 = datestr(t2,formatOut);
+    % Get the channel start and end dates
+    t1 = ch(i).StartDate;
+    t2 = ch(i).EndDate;
     % Get the poles, zeros, and constant
     z = ch(i).Response.Stage(1).PolesZeros.Zero;
     p = ch(i).Response.Stage(1).PolesZeros.Pole;
     k = double(ch(i).Response.Stage(1).PolesZeros.NormalizationFactor)*...
         double(ch(i).Response.InstrumentSensitivity.Value);
     % Format the PZ filename
-    pzFile = sprintf('SAC_PZs_%s_%s_%s.%d',network,station,...
-             ch(i).ChannelCode,i);
-    % Save the PZ file
+    if isempty(ch(i).LocationCode)
+        pzFile = sprintf('SAC_PZs_%s_%s_%s.%d',network,station,...
+                 ch(i).ChannelCode,i);
+    else
+        pzFile = sprintf('SAC_PZs_%s_%s_%s_%s.%d',network,station,...
+                 ch(i).ChannelCode,ch(i).LocationCode,i);
+    end
+    % Save data to the PZ file
     fID = fopen(pzFile,'w');
+    % Write the start and end dates
+    fprintf(fID,sprintf('* Start date: %s\n',t1));
+    fprintf(fID,sprintf('* End date:   %s\n',t2));
+    % Save the poles, zeros, and constant
     fprintf(fID,sprintf('ZEROS %d\n',length(z)));
     for j = 1:length(z)
         fprintf(fID,sprintf('%+e %+e\n',real(z(j)),imag(z(j))));
@@ -67,6 +68,9 @@ for i = 1:length(ch)
     fprintf(fID,sprintf('CONSTANT %e',k));
     fclose(fID);
 end
+
+% Once you have the instrument response files, get some earthquake data!
+
 
 %%
 
@@ -133,3 +137,48 @@ z = ch(1).Response.Stage(1).PolesZeros.Zero;
 p = ch(1).Response.Stage(1).PolesZeros.Pole;
 k = double(ch(1).Response.Stage(1).PolesZeros.NormalizationFactor) * ...
     double(ch(1).Response.InstrumentSensitivity.Value);
+
+%% Make Bode plots
+
+[z,p,k] = parsePZ('/Users/aburky/PycharmProjects/bermudaRFs/matlab/SAC_PZs_TA_O61A_BHZ.6');
+
+npts = 10000000;
+nfft = 2^nextpow2(npts);
+nfreq = (nfft / 2) + 1;
+f = linspace(0,40,nfreq);
+
+[b,a] = zp2tf(z,p,k);
+[h,w] = freqs(b,a,2*pi*f);
+
+% Amplitude response plot
+figure(1)
+loglog(f,abs(h),'r','linewidth',1)
+hold on
+plot([20 20],[1e0 1e10],'k--')
+xlim([1e-5 40])
+ylim([5e3 3e9])
+grid on
+title('TA.O61A..BHZ: Amplitude Response')
+ylabel('Gain (Counts / m/s)')
+xlabel('Frequency (Hz)')
+ax = gca;
+ax.FontSize = 12;
+ax.TickDir = 'out';
+set(gcf,'Position',[0 0 600 400])
+
+% Phase response plot
+figure(2)
+semilogx(f,((180/pi) * angle(h)),'b','linewidth',1)
+hold on
+plot([20 20],[-200 200],'k--')
+xlim([1e-5 40])
+ylim([-181 180])
+grid on
+title('TA.061A..BHZ: Phase Response')
+ylabel('Phase ($^{\circ}$)')
+xlabel('Frequency (Hz)')
+ax = gca;
+ax.FontSize = 12;
+ax.TickDir = 'out';
+ax.YTick = [-180, -135, -90, -45, 0, 45, 90, 135, 180];
+set(gcf,'Position',[650 0 600 400])
