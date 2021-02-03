8184897b79147be115ed682031283b06b38cc11c
Alex Burky <burk4y@gmail.com>
2021-02-02 20:43:43 -0500

    Syncing updates

--- matlab/fetchRFQuakes.m
+++ matlab/fetchRFQuakes.m
@@ -7,7 +7,7 @@
 % to their corresponding poles and zeros data.
 %
 %--------------------------------------------------------------------------
-% Last updated 1/28/2020 by aburky@princeton.edu
+% Last updated 2/2/2021 by aburky@princeton.edu
 %--------------------------------------------------------------------------
 
 clear,clc
