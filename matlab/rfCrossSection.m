a5d70797b0fe7a03fcc75416c3de27b0ab891281
Alex Burky <burk4y@gmail.com>
2021-04-26 10:54:54 -0400

    Syncing updates

--- matlab/rfCrossSection.m
+++ matlab/rfCrossSection.m
@@ -3,18 +3,21 @@
 % This script produces cross sectional plots through a volume of
 % receiver function data for the Eastern US.
 %
+% Beta: Adding an option to overlay the cross section on a tomographic
+%       cross section?
+%
 % Inputs: Start and end coordinates of cross section?
 %
 %------------------------------------------------------------------
-% Last updated 2/11/2021 by aburky@princeton.edu
+% Last updated 4/22/2021 by aburky@princeton.edu
 %------------------------------------------------------------------
 
 clear,clc
 
 % Location of Depth Converted Stacks
 stackDir = ['/Users/aburky/IFILES/NETWORKS/TA_Analysis/',...
-            'Stacks/FILTERED_0.2_5/GW20/'];
-stacks = dir(fullfile(stackDir,'*.mat'));
+            'Stacks/FILTERED_0.02_0.2/GW10/'];
+stacks = dir(fullfile(stackDir,'N*.mat'));
 
 % Location of station latitude abd longitude information
 metaDir = '/Users/aburky/GFILES/TA_Website';
@@ -196,3 +199,121 @@ for stat = 1:length(stats)
     end
 
 end
+
+%% Load in the SEMUCB Tomography Model
+
+% Load model SEMUCB_wm1 (do this once, it takes a long time!)
+load('/Users/aburky/IFILES/MODELS/SEMUCB_WM1/SEMUCBwm1_interpolant.mat');
+
+% Load the colormap for plotting!
+load('/Users/aburky/MFILES/colormaps/vik.mat');
+
+%% (Beta Zone) Overlay the cross section on a tomographic plot
+
+% Get the minimum and maximum longitude of current batch of RFs
+minLon = rf{1}.lon;
+minLat = rf{1}.lat;
+maxLon = rf{1}.lon;
+maxLat = rf{1}.lat;
+for i = 1:length(rf)
+    if rf{i}.lon < minLon
+        minLon = rf{i}.lon;
+        minLat = rf{i}.lat;
+    end
+    if rf{i}.lon > maxLon
+        maxLon = rf{i}.lon;
+        maxLat = rf{i}.lat;
+    end
+end
+
+%% Part 1a: Prepare tomography data for plotting
+
+degrees = [minLat; minLon; maxLat; maxLon];
+
+% Desired number of depth points (resolution)
+n = 500;
+
+% Depth range to plot
+minDep = 300;
+maxDep = 750;
+
+% Desired depths to plot
+dep = linspace(minDep, maxDep, n);
+
+% Generate lat-lon grid
+[lat,lon] = gcwaypts(degrees(1),degrees(2),...
+                     degrees(3),degrees(4),n-1);
+                    
+% Create a grid of model values for each (lat,lon,depth)
+absVs = zeros(n,n);
+for j = 1:n
+    [x,y,z_t] = sph2cart((pi/180)*lon(j),...
+                        (pi/180)*lat(j),6371-dep);
+    absVs(j,:) = Vs(x,y,z_t);
+end
+
+% Pre-allocate variables
+meanVs = zeros(1, n);
+delVs = zeros(n, n);
+
+% Construct array of averages for each depth slice
+for j = 1:n
+    meanVs(j) = mean(absVs(:,j));
+    % Replace absolute velocities with relative velocities
+    delVs(:,j) = ((absVs(:,j)-meanVs(j))./meanVs(j))*100;
+end
+
+% Rotate the velocity grid to give it the right sense
+flipVs = transpose(delVs);
+rotVs = rot90(flipVs,2);
+
+%% Part 1b: Plot the tomographic cross section
+
+% Generate x and y coordinates consistent with RF data
+y = linspace(minDep,maxDep,n);
+x = linspace(minLon-1,maxLon+1,n);
+
+% pcolor(x,y,rotVs);
+% shading interp
+% hold on
+% [C,H] = contour(x,y,rotVs,0,'k');
+[C,H] = contourf(x,y,fliplr(flipud(rotVs)),50,'EdgeColor','none');
+ax1 = gca;
+ax1.YDir = 'reverse';
+set(H,'linewidth',0.01)
+caxis([-2 2])
+colormap(flipud(vik))
+h = colorbar('location','southoutside');
+set(h,'TickLabelInterpreter','latex')
+% set(h,'position',[0.4 0.05 0.25 0.05])
+ylabel(h,'$\delta V_{S}$ from Section 1-D Average (\%)')
+h.Label.Interpreter = 'latex';
+hold on
+
+%% Part 2: Overlay the receiver functions
+scale = 10;
+for i = 1:length(rf)
+    pos = scale*rf{i}.d + rf{i}.lon;
+    pos(pos < rf{i}.lon) = rf{i}.lon;
+    xPatchPos = [rf{i}.lon * ones(size(pos)), fliplr(pos)];
+    yPatchPos = [z, fliplr(z)];
+    fill(xPatchPos,yPatchPos,'r','LineStyle','None')
+    hold on
+    neg = scale*rf{i}.d + rf{i}.lon;
+    neg(neg > rf{i}.lon) = rf{i}.lon;
+    xPatchNeg = [rf{i}.lon * ones(size(neg)), fliplr(neg)];
+    yPatchNeg = [z, fliplr(z)];
+    fill(xPatchNeg,yPatchNeg,'b','LineStyle','None')
+    plot(scale*rf{i}.d + rf{i}.lon,z,'k')
+end
+ylim([300 750])
+xlim([minLon-1 maxLon+1])
+ax1 = gca;
+ax1.YDir = 'reverse';
+ax1.TickDir = 'out';
+xlabel('Longitude ($^{\circ}$)')
+ylabel('Depth (km)')
+% title(sprintf('TA Cross Section: Station Code %s*',stats{stat}))
+ax1.FontSize = 12;
+plot([-200 200],[410 410],'k--')
+plot([-200 200],[660 660],'k--')
