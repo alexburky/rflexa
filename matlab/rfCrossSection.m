0a359ba3f0887bd2aa4f63b61883c0e088ba3e58
Alex Burky <burk4y@gmail.com>
2021-05-05 11:37:21 -0400

    Syncing updates

--- matlab/rfCrossSection.m
+++ matlab/rfCrossSection.m
@@ -9,7 +9,7 @@
 % Inputs: Start and end coordinates of cross section?
 %
 %------------------------------------------------------------------
-% Last updated 5/4/2021 by aburky@princeton.edu
+% Last updated 5/5/2021 by aburky@princeton.edu
 %------------------------------------------------------------------
 
 clear,clc
