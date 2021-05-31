160776b78c0b1243ac9952d5252a4e0186b03616
Alex Burky <burk4y@gmail.com>
2021-05-31 11:14:34 -0400

    Syncing updates

---
+++ matlab/ccpStack.m
@@ -0,0 +1,183 @@
+% ccpStack.m
+%
+% This script is used to make CCP stacks of the TA dataset (US Array) with
+% a focus on the mantle transition zone. It is assumed that the data are
+% structured in a specific directory structure, and that the paths to
+% relevant piercing point data are specified. The output of this script is
+% a .mat file with the CCP stack
+%
+%--------------------------------------------------------------------------
+% Last updated 5/28/2021 by aburky@princeton.edu
+%--------------------------------------------------------------------------
+
+clear,clc
+
+% Path to piercing point data
+pcDir{1} = ['/Users/aburky/IFILES/NETWORKS/TA_Analysis/piercePoints/',...
+            'pierceMoho/'];
+pcDir{2} = ['/Users/aburky/IFILES/NETWORKS/TA_Analysis/piercePoints/',...
+            'pierce210/'];
+pcDir{3} = ['/Users/aburky/IFILES/NETWORKS/TA_Analysis/piercePoints/',...
+            'pierce410/'];
+pcDir{4} = ['/Users/aburky/IFILES/NETWORKS/TA_Analysis/piercePoints/',...
+            'pierce660/'];
+
+% Path to receiver function data        
+rfDir = '/Users/aburky/IFILES/NETWORKS/TA/';
+rfDet = '/NULL/RFUNCS_VEL/FILTERED_0.02_0.2/GW10/';        
+        
+%--------------------------------------------------------------------------
+% Load all piercing point data
+%--------------------------------------------------------------------------        
+     
+% Piercing point files
+for i = 1:length(pcDir)
+    pcFile{i} = dir(fullfile(pcDir{i},'*.dat'));
+end
+
+% Read in all piercing point data
+for j = 1:length(pcDir)
+    
+    pcNam = [];
+    pcLat = [];
+    pcLon = [];
+    for i = 1:length(pcFile{j})
+        % pcData{i} = dlmread(fullfile(pcDir,pcFile(i).name),'',0,1);
+        % pcData{i} = importdata(fullfile(pcDir,pcFile(i).name));
+        pcData = readtable(fullfile(pcDir{j},pcFile{j}(i).name));
+        
+        % Only save variables if data isn't empty
+        if ~isempty(pcData)
+            pcNam{i} = pcData.Var1;
+            pcLat{i} = pcData.Var2;
+            pcLon{i} = pcData.Var3;
+        end
+    end
+    
+    file{j} = [];
+    lat{j} = [];
+    lon{j} = [];
+    % Concatenate data?
+    for i = 1:length(pcFile{j})
+        file{j} = [file{j}; pcNam{i}];
+        lat{j} = [lat{j}; pcLat{i}];
+        lon{j} = [lon{j}; pcLon{i}];
+    end
+    
+end
+
+%--------------------------------------------------------------------------
+% Bin the data
+%--------------------------------------------------------------------------
+
+% Define lat/lon limits
+lonBin = -98.5:0.5:-63.5;
+latBin = 22.5:0.5:51.5;
+
+for i = 1:length(pcDir)
+    [N{i},~,~,binX{i},binY{i}] = histcounts2(lon{i},lat{i},lonBin,latBin);
+end
+
+%--------------------------------------------------------------------------
+% Calculate the CCP stack!
+%--------------------------------------------------------------------------
+
+% Define depth increment
+dz = 0.1;
+z = 0:dz:800;
+
+% Stack 410 bits
+nRows = size(N{3},1);
+nCols = size(N{3},2);
+ccp410 = cell(nRows,nCols);
+for i = 1:nRows
+    for j = 1:nCols
+        sbin = [i j];
+        [tf,loc] = ismember([binX{3},binY{3}],sbin,'row');
+        idx = find(loc);
+        
+        % Read in all of the receiver functions in this bin
+        rf = {};
+        ccp410{i,j} = zeros(8001,1);
+        fprintf('[%i %i] N = %i\n',i,j,length(idx))
+        for k = 1:length(idx)
+            rfData = file{3}{idx(k)};
+            tmp = strsplit(rfData,'TA.');
+            stat = extractBefore(tmp{2},'.');
+            
+            % Read in a receiver function
+            rfPath = fullfile(rfDir,stat,rfDet,rfData);
+            [rf{k}.t,rf{k}.d,rf{k}.h] = fread_sac(rfPath);
+            
+            % Get depth conversion data
+            P = rf{k}.h.user(10)/deg2km(1);
+            dt = rf{k}.h.delta;
+            
+            % Depth convert the receiver function
+            rf{k}.dep = rfDepcon(rf{k}.d,dt,P,dz,'iasp91','true');
+            
+            % Add the depth converted receiver function to the stack
+            ccp410{i,j} = ccp410{i,j} + rf{k}.dep;
+            
+        end
+        
+        % Normalize the stack by the number of RFs in the bin
+        ccp410{i,j} = ccp410{i,j}/length(idx);
+    end
+end
+
+% Stack 660 bits
+nRows = size(N{4},1);
+nCols = size(N{4},2);
+ccp660 = cell(nRows,nCols);
+for i = 1:nRows
+    for j = 1:nCols
+        sbin = [i j];
+        [tf,loc] = ismember([binX{4},binY{4}],sbin,'row');
+        idx = find(loc);
+        
+        % Read in all of the receiver functions in this bin
+        rf = {};
+        ccp660{i,j} = zeros(8001,1);
+        fprintf('[%i %i] N = %i\n',i,j,length(idx))
+        for k = 1:length(idx)
+            rfData = file{4}{idx(k)};
+            tmp = strsplit(rfData,'TA.');
+            stat = extractBefore(tmp{2},'.');
+            
+            % Read in a receiver function
+            rfPath = fullfile(rfDir,stat,rfDet,rfData);
+            [rf{k}.t,rf{k}.d,rf{k}.h] = fread_sac(rfPath);
+            
+            % Get depth conversion data
+            P = rf{k}.h.user(10)/deg2km(1);
+            dt = rf{k}.h.delta;
+            
+            % Depth convert the receiver function
+            rf{k}.dep = rfDepcon(rf{k}.d,dt,P,dz,'iasp91','true');
+            
+            % Add the depth converted receiver function to the stack
+            ccp660{i,j} = ccp660{i,j} + rf{k}.dep;
+            
+        end
+        
+        % Normalize the stack by the number of RFs in the bin
+        ccp660{i,j} = ccp660{i,j}/length(idx);
+    end
+end
+
+% Stitch together the 410 and 660 portions
+for i = 1:nCols
+    for j = 1:nRows
+        CCP(i,j,:) = zeros(8001,1);
+        CCP(i,j,2851:5350) = ccp410{j,i}(2851:5350);
+        CCP(i,j,5351:7850) = ccp410{j,i}(2851:5350);
+    end
+end
+
+%--------------------------------------------------------------------------
+% Save the result
+%--------------------------------------------------------------------------
+
+save('CCP_Stack_Filt0.02_0.2_GW10.mat','CCP','ccp410','ccp660',...
+        'latBin','lonBin');
