7d14d33b9d8910d6b890199271e82995ab0aeccd
Alex Burky <burk4y@gmail.com>
2022-05-31 11:12:02 -0400

    Syncing updates

--- matlab/ccpStack.m
+++ matlab/ccpStack.m
@@ -7,7 +7,7 @@
 % a .mat file with the CCP stack
 %
 %--------------------------------------------------------------------------
-% Last updated 5/30/2021 by aburky@princeton.edu
+% Last updated 5/31/2021 by aburky@princeton.edu
 %--------------------------------------------------------------------------
 
 clear,clc
